ef44b8a3539fcbb74c3078faf4a35d58db59fafa
Gabriel <gabrielturing@gmail.com>
2020-09-04 22:55:34 -0300

    Puxar URI origem e destino
    URI para os resultados da origem e destino com todas as informações a respeita da linha

--- emtu.php
+++ emtu.php
@@ -1,5 +1,5 @@
 <?php
-
+$i = 0;
 $municipios = ['Alambari', 'Aluminio', 'Americana', 'Aparecida', 'Aracariguama', 'Aracoiaba da Serra', 'Arapei', 'Areias', 'Artur Nogueira', 'Aruja', 'Bananal', 'Barueri', 'Bertioga', 'Biritiba-mirim', 'Boituva', 'Cacapava', 'Cachoeira Paulista', 'Caieiras', 'Cajamar', 'Campinas', 'Campos do Jordao', 'Canas', 'Capela do Alto', 'Caraguatatuba', 'Carapicuiba', 'Cerquilho', 'Cesario Lange', 'Cosmopolis', 'Cotia', 'Cruzeiro', 'Cubatao', 'Cunha', 'Diadema', 'Embu das Artes', 'Embu-guacu', 'Engenheiro Coelho', 'Ferraz de Vasconcelos', 'Francisco Morato', 'Franco da Rocha', 'Guararema', 'Guaratingueta', 'Guaruja', 'Guarulhos', 'Holambra', 'Hortolandia', 'Ibiuna', 'Igarata', 'Ilhabela', 'Indaiatuba', 'Ipero', 'Itanhaem', 'Itapecerica da Serra', 'Itapetininga', 'Itapevi', 'Itaquaquecetuba', 'Itatiba', 'Itu', 'Jacarei', 'Jaguariuna', 'Jambeiro', 'Jandira', 'Jumirim', 'Juquitiba', 'Lagoinha', 'Lavrinhas', 'Lorena', 'Mairinque', 'Mairipora', 'Maua', 'Mogi das Cruzes', 'Mongagua', 'Monte Mor', 'Monteiro Lobato', 'Morungaba', 'Natividade da Serra', 'Nova Odessa', 'Osasco', 'Paraibuna', 'Paulinia', 'Pedreira', 'Peruibe', 'Piedade', 'Pilar do Sul', 'Pindamonhangaba', 'Piquete', 'Pirapora do Bom Jesus', 'Poa', 'Porto Feliz', 'Potim', 'Praia Grande', 'Queluz', 'Redencao da Serra', 'Ribeirao Pires', 'Rio Grande da Serra', 'Roseira', 'Salesopolis', 'Salto', 'Salto de Pirapora', 'Santa Barbara do Oeste', 'Santa Branca', 'Santa Isabel', 'Santana de Parnaiba', 'Santo Andre', 'Santo Antonio de Posse', 'Santo Antonio do Pinhal', 'Santos', 'Sao Bento do Sapucai', 'Sao Bernardo do Campo', 'Sao Caetano do Sul', 'Sao Jose do Barreiro', 'Sao Jose dos Campos', 'Sao Lourenco da Serra', 'Sao Luiz do Paraitinga', 'Sao Miguel Arcanjo', 'Sao Paulo', 'Sao Roque', 'Sao Sebastiao', 'Sao Vicente', 'Sarapui', 'Silveiras', 'Sorocaba', 'Sumare', 'Suzano', 'Taboao da Serra', 'Tapirai', 'Tatui', 'Taubate', 'Tiete', 'Tremembe', 'Ubatuba', 'Valinhos', 'Vargem Grande Paulista', 'Vinhedo', 'Votorantim'];
 echo 'de;para;numero;preco;linha;empresa;' . "\n";
 foreach ($municipios as $de) {
@@ -17,7 +17,8 @@ foreach ($municipios as $de) {
         $empresa = '/<td class=\"texto\">(.*?)<\/td>/';
         $linha = '/<span style=\"font\-size: 10px\">(.*?)<\/span>/';
         $numero = "/AbreJanelaAvaliacaoLinhas\(\'(.*?)\',\'\/sistemas\/linha/";
-
+        $infoline = "/AbreJanelaAvaliacaoLinhas\(\'([0-9]+','(.*?))\'\)\"/";
+        $servico = "/<tr><td class=\"destaque2\">Serviço:<\/td>(.*?)<td colspan=\"2\">(.*?)<\/td><\/tr>/";
 
         preg_match_all($preco, $get, $matches);
         $preco = $matches[1];
@@ -43,15 +44,24 @@ foreach ($municipios as $de) {
             $linha[$key] = str_replace('<br>', '', $linha[$key]);
             $linha[$key] = trim($linha[$key]);
         }
+        $matches2 = array();
 
-        /*var_dump($numero);
-        var_dump($linha);
-        var_dump($preco);
-        var_dump($empresa);*/
+        preg_match_all($infoline, $get, $matches2);
 
-        foreach ($preco as $key => $value) {
-            echo $de . ';' . $ate['municipio'] . ';' . $numero[$key] . ';' . $preco[$key] . ';' . $linha[$key] . ';' . $empresa[$key] . ";\n";
-        }
-        //break (2);
+        var_dump($matches2[2]);
+
+        /*foreach ($preco as $key => $value) {
+            $url2 = 'http://www.emtu.sp.gov.br/sistemas/linha/resultado1.htm?pag=origemdestino.htm&numlinha='.intval($numero[$key]).'&tipo=&rua=';
+            $get2 = file_get_contents($url2);
+            $servico = '/<td class=\"destaque2\">Serviço:<\/td>(.*?)<td colspan=\"2\">(.*?)<\/td>/';
+            preg_match_all($servico, $get2, $matches);
+            $servico = array();
+            var_dump($matches);
+            break(3);
+            //echo $de . ';' . $ate['municipio'] . ';' . $numero[$key] . ';' . $preco[$key] . ';' . $linha[$key] . ';' . $empresa[$key] . ";\n";
+        }*/
+        if(false)
+            break (2);
+        $i++;
     }
 }
